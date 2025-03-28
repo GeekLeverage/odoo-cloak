e958b3eb0f49e142a58d93d21d5e55cd4904970f
Eric Fledderman <eric@ericfledderman.me>
2025-03-28 08:00:32 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -7,14 +7,14 @@ async function handleRequest(request) {
     let text = await response.text()
 
     // Insert JavaScript to remove the element
-    const scriptToRemoveElement = `<script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var elem = document.querySelector('.database_expiration_panel');
-            if (elem) {
-                elem.remove();
-            }
-        });
-    </script>`
+    const scriptToRemoveElement = `
+    <script>
+        var elem = document.querySelector('.database_expiration_panel');
+        if (elem) {
+            elem.remove();
+        }
+    </script>
+    `
 
     // Modify the response to include the JavaScript just before </body>
     text = text.replace('</body>', `${scriptToRemoveElement}</body>`)
