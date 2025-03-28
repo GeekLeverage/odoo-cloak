8ee745e8657b31d8dba2627143951401c8abb914
Eric Fledderman <eric@ericfledderman.me>
2025-03-27 23:15:40 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,14 +6,17 @@ async function handleRequest(request) {
     let response = await fetch(request)
     let text = await response.text()
 
-    // Insert your custom CSS
-    const customCSS = `<style>
-        .database_expiration_panel {
-            display: none;
-        }
-    </style>`
+    // Insert JavaScript to remove the element
+    const scriptToRemoveElement = `<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elem = document.querySelector('.database_expiration_panel');
+            if (elem) {
+                elem.remove();
+            }
+        });
+    </script>`
 
-    // Modify the response to include the CSS
-    text = text.replace('</head>', `${customCSS}</head>`)
+    // Modify the response to include the JavaScript
+    text = text.replace('</head>', `${scriptToRemoveElement}</head>`)
     return new Response(text, response)
 }
